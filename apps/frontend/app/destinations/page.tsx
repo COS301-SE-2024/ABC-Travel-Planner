440c6e2664e80f6af107d8010a3c9123b0a214b4
Bigmanfish1 <igbojinnavictor@gmail.com>
2024-05-29 13:46:19 +0200

    Added pulling data

--- apps/frontend/app/destinations/page.tsx
+++ apps/frontend/app/destinations/page.tsx
@@ -2,6 +2,138 @@
 import React, { useState } from 'react';
 import {  FaBookmark } from 'react-icons/fa';
 
+const countries: string[] = [
+  'Nigeria',
+  'South Africa',
+  'Egypt',
+  'Kenya',
+  'Ghana',
+  'Morocco',
+  'Ethiopia',
+  'Tanzania',
+  'Uganda',
+  'Algeria',
+  'Angola',
+  'Cameroon',
+  'Ivory Coast',
+  'Senegal',
+  'Tunisia',
+  'China',
+  'India',
+  'Japan',
+  'South Korea',
+  'Indonesia',
+  'Pakistan',
+  'Bangladesh',
+  'Vietnam',
+  'Philippines',
+  'Thailand',
+  'Malaysia',
+  'Singapore',
+  'Nepal',
+  'Sri Lanka',
+  'Myanmar',
+  'Germany',
+  'France',
+  'United Kingdom',
+  'Italy',
+  'Spain',
+  'Netherlands',
+  'Sweden',
+  'Norway',
+  'Denmark',
+  'Finland',
+  'Poland',
+  'Belgium',
+  'Austria',
+  'Switzerland',
+  'Greece',
+  'United States',
+  'Canada',
+  'Mexico',
+  'Cuba',
+  'Jamaica',
+  'Haiti',
+  'Dominican Republic',
+  'Guatemala',
+  'Honduras',
+  'El Salvador',
+  'Costa Rica',
+  'Panama',
+  'Belize',
+  'Nicaragua',
+  'Barbados',
+  'Brazil',
+  'Argentina',
+  'Colombia',
+  'Chile',
+  'Peru',
+  'Venezuela',
+  'Ecuador',
+  'Bolivia',
+  'Paraguay',
+  'Uruguay',
+  'Guyana',
+  'Suriname',
+  'French Guiana',
+  'Falkland Islands',
+  'South Georgia and the South Sandwich Islands',
+  'Australia',
+  'New Zealand',
+  'Fiji',
+  'Papua New Guinea',
+  'Samoa',
+  'Tonga',
+  'Vanuatu',
+  'Solomon Islands',
+  'Micronesia',
+  'Palau',
+  'Nauru',
+  'Kiribati',
+  'Marshall Islands',
+  'Tuvalu',
+  'New Caledonia',
+];
+
+async function getData() {
+  const url = 'https://api.content.tripadvisor.com/api/v1/location/search?key=EA30B923BE4A4CB28EE695CDFFEB1DE7&searchQuery=South%20Africa';
+  const options = { method: 'GET', headers: { accept: 'application/json' } };
+
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    const updatedData = await Promise.all(data.data.map(async (destination: any) => {
+      const imageUrl = await fetchImage(destination.location_id);
+      return { ...destination, image: imageUrl };
+    }));
+
+    //return updatedData;
+    return { data: updatedData };
+  } catch (err) {
+    console.error(err);
+  }
+
+}
+
+async function fetchImage(locationId: any) {
+  const imageUrl = `https://api.content.tripadvisor.com/api/v1/location/${locationId}/photos?key=EA30B923BE4A4CB28EE695CDFFEB1DE7`;
+  const options = { method: 'GET', headers: { accept: 'application/json' } };
+
+  try {
+    const response = await fetch(imageUrl, options);
+    const data = await response.json();
+
+    if (data && data.data && data.data.length > 0) {
+      return data.data[0].images.original.url; // Use the first image's medium URL
+    } else {
+      return null; // No image available
+    }
+  } catch (err) {
+    console.error(`Error fetching image for location ${locationId}:`, err);
+    return null; // Return null if there's an error
+  }
+}
 
 const Destinations = () => {
   const [destinations, setDestinations] = useState([
