6abb9f5bf422304b63c7e815ab5ae070ad6e4b7a
kabelokaybee <97983960+kabelokaybee@users.noreply.github.com>
2024-05-29 22:13:29 +0200

    Added a logout button but it still needs styling

--- apps/frontend/app/account/page.tsx
+++ apps/frontend/app/account/page.tsx
@@ -1,14 +1,34 @@
-import { FaMapMarkerAlt, FaRegCalendarAlt, FaPlane, FaEdit, FaQuestionCircle, FaInfoCircle } from 'react-icons/fa';
+"use client";
+
+import {
+  FaMapMarkerAlt,
+  FaRegCalendarAlt,
+  FaPlane,
+  FaEdit,
+  FaQuestionCircle,
+  FaInfoCircle,
+} from "react-icons/fa";
+import logout from "./index";
+import { useRouter } from "next/navigation";
 
 const Account = () => {
+  const router = useRouter();
+  const handleSignout = async () => {
+    await logout();
+    router.push("/login");
+  };
   return (
     <div className="account-container">
       <div className="account-profile">
         <div className="account-profile-image">
           <img src="/Images/profile.jpg" alt="Profile" />
         </div>
-        <h1 className="account-name" style={{ fontSize: '2rem' }}>Jessica</h1>
-        <h2 className="account-email" style={{ fontSize: '1.5rem' }}>jessica.vorster@gmail.com</h2>
+        <h1 className="account-name" style={{ fontSize: "2rem" }}>
+          Jessica
+        </h1>
+        <h2 className="account-email" style={{ fontSize: "1.5rem" }}>
+          jessica.vorster@gmail.com
+        </h2>
       </div>
       <div className="account-info-container">
         <div className="account-info">
@@ -24,13 +44,24 @@ const Account = () => {
             <p>Member Since: 2015 January 1st</p>
           </div>
           <div className="account-info-item">
-            <p>Previous Travel History: Here's a sample travel history description:
-              "I have always had a passion for exploring new places and experiencing different cultures. Some of my most memorable adventures include hiking the Inca Trail to Machu Picchu, exploring the ancient ruins of Rome, and cruising through the picturesque fjords of Norway. Each journey has taught me something new about the world and myself, and I look forward to many more adventures in the future."
+            <p>
+              Previous Travel History: Here's a sample travel history
+              description: "I have always had a passion for exploring new places
+              and experiencing different cultures. Some of my most memorable
+              adventures include hiking the Inca Trail to Machu Picchu,
+              exploring the ancient ruins of Rome, and cruising through the
+              picturesque fjords of Norway. Each journey has taught me something
+              new about the world and myself, and I look forward to many more
+              adventures in the future."
             </p>
           </div>
         </div>
         <div className="account-image-container">
-          <img src="/Images/Tourism.jpg" alt="Your Travel History" className="account-travel-image" />
+          <img
+            src="/Images/Tourism.jpg"
+            alt="Your Travel History"
+            className="account-travel-image"
+          />
         </div>
       </div>
       <div className="account-buttons">
@@ -45,6 +76,10 @@ const Account = () => {
         <button className="account-button">
           <FaInfoCircle className="account-button-icon" />
           About
+        </button>
+
+        <button onClick={handleSignout} className="account-button">
+          Logout
         </button>
       </div>
     </div>
