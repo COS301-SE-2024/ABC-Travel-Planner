bc809ba19815fcc3ff761a163427e39d607cdbe8
Bigmanfish1 <igbojinnavictor@gmail.com>
2024-05-31 16:15:08 +0200

    bookmark icon displays

--- apps/frontend/app/destinations/destinationsCard.tsx
+++ apps/frontend/app/destinations/destinationsCard.tsx
@@ -1,7 +1,8 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaBookmark } from 'react-icons/fa';
 import { createClient } from '../utils/supabase/client';
+import { getCurrentUser } from '../actions/actions';
 interface DestinationCardProps {
     destination: any;
 }
@@ -9,22 +10,39 @@ interface DestinationCardProps {
 const DestinationCard: React.FC<DestinationCardProps> = ({ destination }) => {
     const [isFavourite, setIsFavourite] = useState(destination.isFavourite);
 
+    const handleFavouriteToggle = () => {
+        if(isFavourite) {
+            handleUnFavoriteClick();
+        }else {
+            handleFavoriteClick();
+        }
+    }
     const handleFavoriteClick = async () => {
+        setIsFavourite(true);
         const supabase = createClient();
         const { data: { user } } = await supabase.auth.getUser();
+        
+        // const curruser = await getCurrentUser();
+        // console.log("This is the one we get from the user " + JSON.stringify(curruser));
         const { data, error } = await supabase.from('favourite_destinations').insert([
-                { some_column: '', other_column: 'otherValue' },
+                { user_id: `${user?.id}`, destination_object: destination, location_id: destination.location_id, status: true},
             ])
             .select()
-        const newStatus = !isFavourite;
-        setIsFavourite(newStatus);
+        
+    };
+
+    const handleUnFavoriteClick = async () => {
+        setIsFavourite(false);
+        const supabase = createClient();
+        const { data: { user } } = await supabase.auth.getUser();
+        const { data, error } = await supabase.from('favourite_destinations').update({status: false}).eq('location_id', `${destination.location_id}`).eq('user_id', `${user?.id}`);
     };
 
     return (
         <div className="card">
             <img src={destination.image} alt={destination.name} className="image" />
             <p>{destination.address_obj.address_string}</p>
-            <button onClick={handleFavoriteClick} className="favoriteButton">
+            <button onClick={handleFavouriteToggle} className="favoriteButton">
                 {isFavourite ? <FaBookmark style={{ color: 'yellow' }} /> : <FaBookmark />}
             </button>
         </div>
