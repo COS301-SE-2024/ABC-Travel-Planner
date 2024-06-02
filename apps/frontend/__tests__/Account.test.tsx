c3aaf1571ee7232a2f25bdf40c77d85425b1f2f2
AidanG19 <aidan.govender2003@gmail.com>
2024-06-02 22:38:46 +0200

    vic merger

--- apps/frontend/__tests__/Account.test.tsx
+++ apps/frontend/__tests__/Account.test.tsx
@@ -3,7 +3,7 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Account from '../app/account/page';
 import { useRouter } from 'next/navigation';
-import logout from '../app/account/index';
+import {logout} from '../app/account/index';
 
 jest.mock('next/navigation', () => ({
   useRouter: jest.fn(),
