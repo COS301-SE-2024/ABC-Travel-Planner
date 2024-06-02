991aef684b9575d5085288489163eafb3f7ee736
kabelokaybee <97983960+kabelokaybee@users.noreply.github.com>
2024-06-02 22:04:43 +0200

    FixProfile

--- apps/frontend/app/account/page.tsx
+++ apps/frontend/app/account/page.tsx
@@ -7,65 +7,114 @@ import {
   FaEdit,
   FaQuestionCircle,
   FaInfoCircle,
-  FaSignOutAlt
+  FaSignOutAlt,
 } from "react-icons/fa";
-import logout from "./index";
+import { logout, getUserProfile } from ".";
 import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 
 const Account = () => {
+  const [profileDetails, setProfileDetails] = useState<{
+    name: string;
+    surname: string;
+    email: string;
+  }>({ name: "", surname: "", email: "" });
   const router = useRouter();
   const handleSignout = async () => {
     await logout();
     router.push("/login");
   };
+  useEffect(() => {
+    const fetchProfileDetails = async () => {
+      const response = await getUserProfile();
+      setProfileDetails(response);
+    };
+    fetchProfileDetails();
+  }, []);
+  if (!profileDetails.name) return <div>Loading...</div>;
   return (
-    <div style={{backgroundSize: '100% 100%', backgroundPosition: 'center', backgroundAttachment: 'fixed', color: '#333', fontFamily: 'Arial, sans-serif', minHeight: '100vh', display: 'flex', flexDirection: 'column', position: 'relative' }}>
-    <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.5)', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-      <div className="account-container" style={{ padding: '20px', borderRadius: '10px', width: '80%' }}>
-        <div className="account-header">
-          <div className="account-profile">
-            <div className="account-profile-image">
-              <img src="/Images/profile.jpg" alt="Profile" />
+    <div
+      style={{
+        backgroundSize: "100% 100%",
+        backgroundPosition: "center",
+        backgroundAttachment: "fixed",
+        color: "#333",
+        fontFamily: "Arial, sans-serif",
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        position: "relative",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          className="account-container"
+          style={{ padding: "20px", borderRadius: "10px", width: "80%" }}
+        >
+          <div className="account-header">
+            <div className="account-profile">
+              <div className="account-profile-image">
+                <img src="/Images/profile.jpg" alt="Profile" />
+              </div>
+              <div className="account-profile-details">
+                <h1 className="account-name">Jessica Vorster</h1>
+                <h2 className="account-email">jessica.vorster@gmail.com</h2>
+                <p className="account-location">
+                  <FaMapMarkerAlt /> South Africa
+                </p>
+                <p className="account-member-since">
+                  <FaRegCalendarAlt /> Member Since: 2015 January 1
+                </p>
+              </div>
             </div>
-            <div className="account-profile-details">
-              <h1 className="account-name">Jessica Vorster</h1>
-              <h2 className="account-email">jessica.vorster@gmail.com</h2>
-              <p className="account-location"><FaMapMarkerAlt /> South Africa</p>
-              <p className="account-member-since"><FaRegCalendarAlt /> Member Since: 2015 January 1</p>
+            <div className="account-actions">
+              <button className="edit-profile-button">
+                <FaEdit /> Edit Profile
+              </button>
             </div>
           </div>
-          <div className="account-actions">
-            <button className="edit-profile-button">
-              <FaEdit /> Edit Profile
-            </button>
-          </div>
-        </div>
-        <div className="account-content">
-          <div className="account-info">
-            <h3 className="info-heading">Previous Travel History</h3>
-            <p className="info-description">
-              I have always had a passion for exploring new places and experiencing different cultures. Some of my most memorable adventures include hiking the Inca Trail to Machu Picchu, exploring the ancient ruins of Rome, and cruising through the picturesque fjords of Norway. Each journey has taught me something new about the world and myself, and I look forward to many more adventures in the future.
-            </p>
-          </div>
-          <div className="account-buttons">
-            <button className="account-button">
-              <FaQuestionCircle /> Help Center
-            </button>
-            <button className="account-button">
-              <FaInfoCircle /> About
-            </button>
-            <button onClick={handleSignout} className="account-button">
+          <div className="account-content">
+            <div className="account-info">
+              <h3 className="info-heading">Previous Travel History</h3>
+              <p className="info-description">
+                I have always had a passion for exploring new places and
+                experiencing different cultures. Some of my most memorable
+                adventures include hiking the Inca Trail to Machu Picchu,
+                exploring the ancient ruins of Rome, and cruising through the
+                picturesque fjords of Norway. Each journey has taught me
+                something new about the world and myself, and I look forward to
+                many more adventures in the future.
+              </p>
+            </div>
+            <div className="account-buttons">
+              <button className="account-button">
+                <FaQuestionCircle /> Help Center
+              </button>
+              <button className="account-button">
+                <FaInfoCircle /> About
+              </button>
+              <button onClick={handleSignout} className="account-button">
                 <FaSignOutAlt /> Logout
               </button>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-
-  
   );
 };
 
 export default Account;
-
