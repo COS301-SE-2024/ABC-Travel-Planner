63badf624bd6e8a563fa0849387d7e367d6fc1e5
AidanG19 <aidan.govender2003@gmail.com>
2024-06-02 22:34:48 +0200

    merger

--- apps/frontend/app/account/index.ts
+++ apps/frontend/app/account/index.ts
@@ -1,7 +1,22 @@
 "use server";
+import readUser from "@/libs/actions";
 import createSupabaseServerClient from "@/libs/supabase/server";
 
-export default async function logout() {
+export async function logout() {
   const supabase = await createSupabaseServerClient();
   await supabase.auth.signOut();
+}
+
+export async function getUserProfile()
+{
+  const result = await readUser();
+  const {
+    data: { user },
+  } = JSON.parse(result);
+
+  return {
+    name: user?.user_metadata?.name || "",
+    surname: user?.user_metadata?.surname || "",
+    email: user?.email || "",
+  };
 }
