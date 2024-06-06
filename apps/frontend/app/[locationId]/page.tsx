e3a42c0dc7b4e2dba734ced285ee5a313101105b
MishkaD <mishka.dukhanti@gmail.com>
2024-06-06 17:32:09 +0200

    icons

--- apps/frontend/app/[locationId]/page.tsx
+++ apps/frontend/app/[locationId]/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import React from 'react';
 import ReviewButton from './ReviewButton'; 
+import { FaGoogle,FaAtlas } from 'react-icons/fa';
 const TouristPage = () => {
   return (
     
@@ -19,7 +20,10 @@ const TouristPage = () => {
       </div>
       <div className="info-section">
         <div className="google-street-view">
-          <h1>Google Earth View</h1>
+          <div className="flex items-center">
+            <FaGoogle size={22} className="mr-2"  style={{ marginBottom: 10 }}/>
+            <h1>Google Earth View</h1>
+          </div>
           <iframe
             src="https://earth.google.com/web/@48.8583701,2.2944813,146.72686635a,666.61608691d,35y,222.03759349h,45t,0r/data=Ck8aTRJHCiUweDQ3ZTY2ZTI5NjRlMzRlMmQ6MHg4ZGRjYTllZTM4MGVmN2UwGZ-uSRLfbUhAIb1EBgMZWwJAKgxUb3JyZSBFaWZmZWwYAiABOgMKATA"
             width="100%"
@@ -30,7 +34,10 @@ const TouristPage = () => {
           ></iframe>
         </div>
         <div className="google-maps-api">
+        <div className="flex items-center">
+        <FaAtlas size={22} className="mr-2"  style={{ marginBottom: 10 }}/>
           <h1>Google Maps API</h1>
+          </div>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.9999022047975!2d2.2944813156743517!3d48.85837007928715!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66efef2e091ab%3A0x40b82c3688c9460!2sEiffel%20Tower!5e0!3m2!1sen!2sfr!4v1625840843485!5m2!1sen!2sfr"
             width="100%"
