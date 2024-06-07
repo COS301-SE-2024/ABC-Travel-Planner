7ca4453f1af748502e348064208f0a61a972798d
Sud0man1 <calvyntbx@gmail.com>
2024-06-08 00:44:19 +0200

    Height issue fix
    Fixed height issue. Remove div button still not working

--- apps/frontend/app/itinerary-items/DynamicDivs.tsx
+++ apps/frontend/app/itinerary-items/DynamicDivs.tsx
@@ -26,8 +26,8 @@ const DynamicDivs: React.FC = () => {
     return (
         <>
         {divs.map((divItem) => (
-            <div key={divItem.id} className="relative border-2 border-black-500 rounded-md bg-gray-200 item-div">
-            <Button className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 focus:outline-none closeButton" onClick={() => handleRemoveDiv(divItem.id)}>
+            <div key={divItem.id} className="relative border-2 border-black-500 rounded-md item-div">
+            <Button className="absolute top-2 right-2 text-gray-800 hover:text-gray-700 focus:outline-none closeButton" onClick={() => handleRemoveDiv(divItem.id)}>
                 <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
                 </svg>
