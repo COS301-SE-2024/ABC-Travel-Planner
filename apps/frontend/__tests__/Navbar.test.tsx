30efe6ababea0efc3a016556c14c7651978fee09
MishkaD <mishka.dukhanti@gmail.com>
2024-06-22 21:59:27 +0200

    testing

--- apps/frontend/__tests__/Navbar.test.tsx
+++ apps/frontend/__tests__/Navbar.test.tsx
@@ -10,16 +10,12 @@ describe('Navbar', () => {
 
     const homeLink = screen.getByText('Home');
     const searchLink = screen.getByText('Search');
-    const destinationsLink = screen.getByText('Destinations');
     const itineraryLink = screen.getByText('Itinerary');
-    const bookingLink = screen.getByText('Booking');
     const accountLink = screen.getByText('Account');
 
     expect(homeLink).toBeInTheDocument();
     expect(searchLink).toBeInTheDocument();
-    expect(destinationsLink).toBeInTheDocument();
     expect(itineraryLink).toBeInTheDocument();
-    expect(bookingLink).toBeInTheDocument();
     expect(accountLink).toBeInTheDocument();
   });
 });
