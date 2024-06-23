30b1e2978074b5e0670b5b0b379a986a0803241d
kabelokaybee <97983960+kabelokaybee@users.noreply.github.com>
2024-06-23 19:35:51 +0200

    Added more tests

---
+++ apps/frontend/cypress/e2e/search.cy.js
@@ -0,0 +1,23 @@
+describe("Search", () => {
+  beforeEach(() => {
+    cy.login("kabelomotloung95@gmail.com", "Password#1234");
+  });
+
+  it("Should display Search page", () => {
+    cy.visit("/search");
+    cy.get(`[data-testid="searchContainer"]`).should("exist");
+  });
+
+  it("Should display search options", () => {
+    cy.visit("/search");
+    cy.get(`[data-testid="searchOptions"]`).should("exist");
+  });
+  it("Should display search input and should be able to type", () => {
+    cy.visit("/search");
+    cy.get(`[data-testid="flightsButton"]`).click({ force: true });
+    cy.get(`[data-testid="searchInput"]`).should("exist");
+    cy.get(`[data-testid="searchButton"]`).should("exist");
+    cy.get(`[data-testid="searchInput"]`).type("South Africa");
+    cy.get(`[data-testid="searchButton"]`).click({ force: true });
+  });
+});
