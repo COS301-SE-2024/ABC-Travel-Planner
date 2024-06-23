fb0f6ee26f442f669c06b805aca583e6be520b24
kabelokaybee <97983960+kabelokaybee@users.noreply.github.com>
2024-06-23 18:23:42 +0200

    Added more integration tests

--- apps/frontend/app/home/DestinationCard.tsx
+++ apps/frontend/app/home/DestinationCard.tsx
@@ -12,13 +12,13 @@ const DestinationCard = ({ destination }: { destination: destination }) => {
   const { name, image, city = 'Unknown', location_id = '0', description = 'Description not available' } = destination;
 
   return (
-    <div className="destination-card bg-white rounded-lg shadow-md overflow-hidden">
-      <Link href={`/${location_id}`} passHref>
-        <img src={image} alt={name} className="w-full h-32 sm:h-48 object-cover"/>
+    <div data-testid="destinationCard" className="destination-card bg-white rounded-lg shadow-md overflow-hidden">
+      <Link data-testid="DestinationLink" href={`/${location_id}`} passHref>
+        <img data-testid="destinationImage" src={image} alt={name} className="w-full h-32 sm:h-48 object-cover"/>
         <div className="p-4">
-          <h3 className="text-lg font-bold mb-2">{name}</h3>
+          <h3 data-testid="destinationName" className="text-lg font-bold mb-2">{name}</h3>
           <p className="text-gray-700 text-md mb-1">{city}</p>
-          <p className="text-gray-600 text-md">{description}</p>
+          <p data-testid="destinationDescription" className="text-gray-600 text-md">{description}</p>
         </div>
       </Link>
     </div>  
--- apps/frontend/cypress/e2e/itinerary.cy.js
+++ apps/frontend/cypress/e2e/itinerary.cy.js
@@ -2,22 +2,21 @@ describe("Create an itinerary", () => {
   beforeEach(() => {
     cy.login("kabelomotloung95@gmail.com", "Password#1234");
   });
-  it("should allow display home page", () => {
+  it("should display the home page", () => {
+    cy.url().should("include", "/home");
+  });
+  it("should display the itinerary page", () => {
     cy.visit("/itinerary");
     cy.get(`[data-testid="itinerariesTitle"]`).should("exist");
     cy.get(`[data-testid="itinerariesTitle"]`).should("contain", "My Itineraries");
     cy.get(`[data-testid="addItineraryButton"]`).should("exist");
+  });
+  it("should allow user to add a new itinerary", () => {
+    cy.visit("/itinerary");
     cy.get(`[data-testid="addItineraryButton"]`).click({ force: true});
-
     cy.get(`[data-testid="ItineraryForm"]`).should("exist");
     cy.get(`[data-testid="itineraryNameInput"]`).type("My first itinerary");
     cy.get(`[data-testid="locationInput"]`).type("USA");
     cy.get(`[data-testid="addItinerarySubmit"]`).click({ force: true});
-
-
-    
-
-
-
   });
 });
