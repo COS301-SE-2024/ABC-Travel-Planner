15d86b7612580e0282b8dc190e488cd974bf53e8
Sud0man1 <calvyntbx@gmail.com>
2024-07-09 21:24:11 +0200

    Parent included

---
+++ apps/frontend/app/itinerary-items/ParentContainer.tsx
@@ -0,0 +1,50 @@
+"use client"
+import React, { useState } from 'react';
+import DynamicDivs from './DynamicDivs';
+import SearchModal from './SearchModal';
+
+interface ParentComponentProps {
+    image_url: string;
+}
+
+const ParentComponent: React.FC<ParentComponentProps> = () => {
+  const [divs, setDivs] = useState<DivItem[]>([]);
+  const [fetchedData, setFetchedData] = useState<ItemData[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  interface ItemData {
+    Item_Name: string;
+    Item_Type: string;
+    image_link: string;
+  }
+
+  interface DivItem {
+    id: number;
+    data: ItemData;
+  }
+
+  const handleAddDiv = (searched: boolean) => {
+    if (searched) {
+      const newId = divs.length;
+      const newDiv = {
+        id: newId,
+        data: fetchedData[newId % fetchedData.length],
+      };
+  
+      setDivs([...divs, newDiv]);
+    }
+  };
+  
+    const handleRemoveDiv = (id: number) => {
+      setDivs(divs.filter(divItem => divItem.id !== id));
+    };
+
+  return (
+    <div>
+      <DynamicDivs passedDivs={divs} handleRemoveDiv={handleRemoveDiv} />
+      <SearchModal handleAddDiv={handleAddDiv} />
+    </div>
+  );
+};
+
+export default ParentComponent;
