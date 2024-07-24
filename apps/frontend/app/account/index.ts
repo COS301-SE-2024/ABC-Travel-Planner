a20f5cd9c566230a9c859823cb81e1d41d9b8db0
kabelokaybee <97983960+kabelokaybee@users.noreply.github.com>
2024-07-24 19:50:30 +0200

    Added some functions for profile page

--- apps/frontend/app/account/index.ts
+++ apps/frontend/app/account/index.ts
@@ -7,16 +7,60 @@ export async function logout() {
   await supabase.auth.signOut();
 }
 
-export async function getUserProfile()
-{
+export async function getUserProfile() {
   const result = await readUser();
+  const supabase = await createSupabaseServerClient();
+
   const {
     data: { user },
   } = JSON.parse(result);
+  const { data: userData } = await supabase
+    .from("Users")
+    .select()
+    .eq("email", user?.email);
+  const { data } = supabase.storage
+    .from("Profile Pictures")
+    .getPublicUrl(`${userData?.at(0).name}_${userData?.at(0).surname}.jpg`);
 
   return {
-    name: user?.user_metadata?.name || "",
-    surname: user?.user_metadata?.surname || "",
-    email: user?.email || "",
+    name: userData?.at(0)?.name || "",
+    surname: userData?.at(0)?.surname || "",
+    email: userData?.at(0)?.email || "",
+    createdAt: userData?.at(0)?.created_at || "",
+    country: userData?.at(0)?.country || "",
+    imageUrl: data?.publicUrl || "",
   };
+}
+
+export async function updateUserProfile(
+  name: string,
+  surname: string,
+  email: string
+) {
+  const supabase = await createSupabaseServerClient();
+  const result = await readUser();
+
+  const {
+    data: { user },
+  } = JSON.parse(result);
+  if (user?.email != email) {
+    const { data, error } = await supabase.auth.updateUser({
+      email: email,
+    });
+  }
+
+  await supabase
+    .from("Users")
+    .update({ name: name, surname: surname, email: email })
+    .eq("user_id", user?.id);
+}
+
+export async function getSharedItineraries() {
+  const supabase = await createSupabaseServerClient();
+  const { data: sharedItineraries } = await supabase
+    .from("Itinerary")
+    .select()
+    .eq("shared", true);
+  console.log(sharedItineraries);
+  return sharedItineraries;
 }
