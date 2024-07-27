8d6bddff83733b1b12c1a1a2e60262209cf81bae
kabelokaybee <97983960+kabelokaybee@users.noreply.github.com>
2024-07-27 12:37:09 +0200

    Updated the middleware

--- apps/frontend/middleware.ts
+++ apps/frontend/middleware.ts
@@ -1,5 +1,7 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
-import { NextResponse, type NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+
+
 
 export async function middleware(request: NextRequest) {
   let response = NextResponse.next({
@@ -8,61 +10,19 @@ export async function middleware(request: NextRequest) {
     },
   });
 
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return request.cookies.get(name)?.value;
-        },
-        set(name: string, value: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value,
-            ...options,
-          });
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          });
-          response.cookies.set({
-            name,
-            value,
-            ...options,
-          });
-        },
-        remove(name: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value: "",
-            ...options,
-          });
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          });
-          response.cookies.set({
-            name,
-            value: "",
-            ...options,
-          });
-        },
-      },
-    }
-  );
+  
+  const cookies = request.cookies;
+  const userId = cookies.get("user_id");
+  console.log("user_id", userId);
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-  if (!user && request.nextUrl.pathname !== "/login") {
+ ///need to check if user is logged in or not
+
+  if (!userId && request.nextUrl.pathname !== "/login") {
     const loginUrl = new URL("/login", request.nextUrl.origin);
     console.log("url", loginUrl.toString());
     return NextResponse.redirect(loginUrl);
   } else if (
-    user &&
+    userId &&
     (request.nextUrl.pathname === "/login" || request.nextUrl.pathname === "/")
   ) {
     const homeUrl = new URL("/home", request.nextUrl.origin);
