47b90ab4418a299f914ffa7f344d367af98b49de
Bigmanfish1 <igbojinnavictor@gmail.com>
2024-08-04 16:28:44 +0200

    auth fixes

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 declare const module: any;
 
 async function bootstrap() {
+
+  const base64Credentials = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+  if (base64Credentials) {
+    const decodedCredentials = Buffer.from(base64Credentials, 'base64').toString('utf8');
+    fs.writeFileSync('/tmp/google-credentials.json', decodedCredentials);
+    process.env.GOOGLE_APPLICATION_CREDENTIALS = '/tmp/google-credentials.json';
+  }
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['http://localhost:3000', 'https://abctravelplanner-8a6f6247b848.herokuapp.com'], 
