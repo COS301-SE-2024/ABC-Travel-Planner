61cb40521952bc5507977a001eda3665aae684dd
Bigmanfish1 <igbojinnavictor@gmail.com>
2024-08-04 13:14:31 +0200

    port adjustment

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,12 +6,13 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://abctravelplanner-8a6f6247b848.herokuapp.com'], // Your Next.js app's URL
+    origin: ['http://localhost:3000', 'https://abctravelplanner-8a6f6247b848.herokuapp.com'], 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
   
-  await app.listen(4000);
+  const port = process.env.PORT || 4000; // Default to 4000 if PORT is not set
+  await app.listen(port);
 
   if (module.hot) {
     module.hot.accept();
