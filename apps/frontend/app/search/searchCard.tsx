48a41eab161d5df70d90ac46fd3194adfcde0aac
Sud0man1 <calvyntbx@gmail.com>
2024-08-11 00:45:17 +0200

    filter test & format correction

--- apps/frontend/app/search/searchCard.tsx
+++ apps/frontend/app/search/searchCard.tsx
@@ -143,9 +143,9 @@ const SearchCard: React.FC<SearchCardProps> = ({ place }) => {
             parsedItem = {}; // or provide default values
         }
         console.log(JSON.stringify({ user_id: user_id, item_name: place.displayName, item_type: place.type, 
-            location: parsedItem.location, itinerary_id: parsedItem.id, destination: place.formattedAddress, image_url: place.firstPhotoUrl, price: place.price, dates: selectedDates}));
+            location: parsedItem.location, itinerary_id: parsedItem.id, destination: place.formattedAddress, image_url: place.firstPhotoUrl, price: place.price, date: selectedDates}));
         const newItem = await axios.post(`${backendUrl}/itinerary-items/add`,{ user_id: user_id, item_name: place.displayName, item_type: place.type, 
-            location: parsedItem.location, itinerary_id: parsedItem.id, destination: place.formattedAddress, image_url: place.firstPhotoUrl, price: place.price, dates: selectedDates});
+            location: parsedItem.location, itinerary_id: parsedItem.id, destination: place.formattedAddress, image_url: place.firstPhotoUrl, price: place.price, date: selectedDates});
     };
 
     const handleSaveNewItinerary = async () => {
@@ -159,7 +159,7 @@ const SearchCard: React.FC<SearchCardProps> = ({ place }) => {
         //Now add the item
         const newItemData = JSON.parse(newI.config.data);
         const newItem = await axios.post(`${backendUrl}/itinerary-items/add`,{ user_id: user_id, item_name: place.displayName, item_type: place.type, 
-            location: newItemData.location, itinerary_id: newI.data, destination: place.formattedAddress, image_url: place.firstPhotoUrl, price: place.price, dates: selectedDates});
+            location: newItemData.location, itinerary_id: newI.data, destination: place.formattedAddress, image_url: place.firstPhotoUrl, price: place.price, date: selectedDates});
     };
 
     function extractLocation(fullString: string) {
