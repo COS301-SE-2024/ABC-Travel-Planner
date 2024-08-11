4d2c994a199b914de3059b165100432607c97f0c
Sud0man1 <calvyntbx@gmail.com>
2024-08-11 16:33:09 +0200

    removed console.log

--- apps/frontend/app/utils/functions/insertRecord.ts
+++ apps/frontend/app/utils/functions/insertRecord.ts
@@ -1,6 +1,7 @@
 export const insertRecord = async (uploadDetails: any) => {
     const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL
     const response = await fetch(`${backendUrl}/itinerary-items/add`, {
+    
     method: 'POST',
     headers: {
         'Content-Type': 'application/json'
@@ -8,6 +9,5 @@ export const insertRecord = async (uploadDetails: any) => {
     body: JSON.stringify(uploadDetails)
     });
 
-    console.log("Response from server: ", JSON.stringify(response));
     return response.status;
 }
