e892643b4d12b4486b11ac3dcab1796983a5e1c2
Bigmanfish1 <igbojinnavictor@gmail.com>
2024-08-11 17:14:01 +0200

    minor fix

--- apps/frontend/app/utils/functions/insertRecord.ts
+++ apps/frontend/app/utils/functions/insertRecord.ts
@@ -1,6 +1,5 @@
 export const insertRecord = async (uploadDetails: any) => {
-    const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL
-    const response = await fetch(`${backendUrl}/itinerary-items/add`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/itinerary-items/add`, {
     
     method: 'POST',
     headers: {
