871d518cf87f79bbb306522cabedeb73a2ef29d4
Calvyn-Van-Wyngaardt <calvyntbx@gmail.com>
2024-09-24 14:59:02 +0200

    Revert "Pulling dev into fix/EmailAddition"
    This reverts commit bee9d05671b6325f3d7ab896d5856b9846d2bdbe.

--- apps/frontend/app/itinerary-items/page.tsx
+++ apps/frontend/app/itinerary-items/page.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import BookMarkComponent from "./BookMarkComponent";
 import "./modal.css";
 import DynamicDivs from "./DynamicDivs";
-import BackButton from "../../public/back.svg";
 
 const getCoordinates = async (location: string) => {
   const encodedAddress = encodeURIComponent(location);
@@ -31,40 +30,52 @@ const ItineraryItems = async ({ searchParams }: { searchParams: { id?: any; loca
 
   return (
     <>
-    <div className="relative flex flex-col space-x-1 justify-center items-center text-center bg-neutral-300">
-      <div className="flex flex-col border border-gray-300 rounded-lg bg-white shadow-md w-[96vw] h-auto iteneraryInfo itineraryItemsBackground">
-      <div className="h-full w-full"> 
-      <div className="flex items-center justify-center bg-slate-50 rounded-sm border-t-orange-500">
-        <h1 className="mb-2 text-4xl font-medium font-['Roboto'] text-black mt-6 w-fit iteneraryHeader backdrop-filter backdrop-blur-[2px] backdrop-contrast-100 rounded-lg"  style={{  marginBottom:20 }}>Itinerary Items:</h1>
-        
-        <div className="absolute left-2 px-4 justify-center items-center">
-            {/* <BookMarkComponent /> */}
-              <Link href={`/itinerary`}>
-                <Button className="rounded-full left-2 backButton">
-                </Button>
-              </Link>
+    <div className="relative flex flex-col space-x-1 justify-center items-center">
+    <div className="flex flex-col border border-gray-300 rounded-lg p-4 bg-white shadow-md w-[96vw] h-auto iteneraryInfo">
+      <h1 className="mb-2 text-2xl font-bold text-gray-800 iteneraryHeader "  style={{ fontSize: '2rem', marginBottom:20 }}>Itinerary Items:</h1>
+      {/* <div className="shadow-md p-4 rounded-lg bg-gray-200" style ={{backgroundColor: 'rgba(173, 216, 230, 0.5)'}}>
+      <form >
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+          <div>
+            <label htmlFor="startDate" className="block text-sm font-medium text-gray-800">Start Date of Trip</label>
+            <input
+              type="date"
+              id="startDate"
+              name="startDate"
+              className="mt-1 block w-full border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm outline-none"
+            />
+          </div>
+          <div>
+            <label htmlFor="endDate" className="block text-sm font-medium text-gray-800">End Date of Trip</label>
+            <input
+              type="date"
+              id="endDate"
+              name="endDate"
+              className="mt-1 block w-full border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm outline-none"
+            />
+          </div>
         </div>
+      </form>
+      </div> */}
 
-        
-
-        <div className='mt-10 w-100 h-10'>
-        <div className="absolute top-8 right-8 px-4">
+      <div className="flex justify-center">
+        <div className="grid grid-cols-auto sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 iteneraries-grid rounded-lg h-full sm:h-auto text-gray-800">
+            <DynamicDivs id={id} location={location} destination={destination} />
+        </div>
+      </div>
+      
+      <div className='mt-10 w-100 h-10'>
+        <div className="absolute bottom-4 right-4 px-4">
         <Link href={`/booking?id=${id}`}>
-          <Button className="border-2 border-black-500 rounded-md doneButton bg-sky-500">
+          <Button className="border-2 border-black-500 rounded-md doneButton bg-blue-700">
             Done
           </Button>
         </Link>
         </div>
-      </div>
-      </div>
 
-      <div className="flex justify-center">
-        <div className="ml-2 mr-2">
-            <div className="grid grid-cols-auto sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 iteneraries-grid rounded-lg h-full sm:h-auto text-gray-800">
-              <DynamicDivs id={id} location={location} destination={destination} />
-            </div>
+        <div className="absolute bottom-4 left-4 px-8 justify-center items-center">
+            <BookMarkComponent />
         </div>
-      </div>
       </div>
     </div>
   </div>
