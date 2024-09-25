dfcb649c7f935576c7b89135141c2b3f25d56ae1
Bigmanfish1 <igbojinnavictor@gmail.com>
2024-09-25 13:05:11 +0200

    make it use heroku port

--- apps/model/chat.py
+++ apps/model/chat.py
@@ -247,5 +247,7 @@ def process_query():
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
